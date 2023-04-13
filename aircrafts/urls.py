a4bb8180b984a1075f09ca5395d700a298744f31
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2023-04-14 00:22:22 +0300

    post, delete are working

--- aircrafts/urls.py
+++ aircrafts/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 from django.urls import path, include
 
 from .views import (
-        AircraftApiView
+        AircraftApiView,
+        AircraftDetailApiView
 )
 
 urlpatterns = [
         path('', AircraftApiView.as_view()),
+        path('<int:aircraft_idx>/' , AircraftDetailApiView.as_view()),
+
 ]
 
 
--- aircrafts/views.py
+++ aircrafts/views.py
@@ -47,3 +47,79 @@ class AircraftApiView(APIView):
 
 
 
+
+
+class AircraftDetailApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, aircraft_idx): #chande paramater name 
+        '''
+        Helper method to get the object with given aircraft id 
+        '''
+        try:
+            return Aircraft.objects.get( aircraft_id = aircraft_idx )
+        except Aircraft.DoesNotExist:
+            return None
+
+    # 3. Retrieve
+    def get(self, request, aircraft_idx, *args, **kwargs):
+        '''
+        Retrieves the Aircraft with given aircraft id 
+        '''
+        aircraft_instance = self.get_object( aircraft_idx )
+        if not aircraft_instance:
+            return Response(
+                {"res": "Object with aircraft_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = AircraftSerializer( aircraft_instance  )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 4. Update
+    def put(self, request, aircraft_idx, *args, **kwargs):
+        '''
+        Updates the aircraft item with given aircraft_id if exists
+        '''
+        aircraft_instance = self.get_object( aircraft_idx )
+        if not aircraft_instance:
+            return Response(
+                {"res": "Object with aircraft_id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            # i think no need for here, maybe i will delete later...
+            # because the argument came from request
+            'name': request.data.get('name'),
+            'callsign': request.data.get('callsign'),
+            'founded_year': request.data.get('founded_year'),
+            'base_airport': request.data.get('base_airport')
+
+        }
+        serializer = AircraftSerializer(instance = aircraft_instance, data=request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 5. Delete
+    def delete(self, request, aircraft_idx, *args, **kwargs):
+        '''
+        Deletes the aircraft item with given aircraft id if exists
+        '''
+        aircraft_instance = self.get_object(aircraft_idx)
+        if not aircraft_instance:
+            return Response(
+                {"res": "Object with aircraft_id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        aircraft_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
+
+
+
+
