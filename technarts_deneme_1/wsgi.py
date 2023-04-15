85c324a5cd8d263e439c0812bcfdd299099cf2bb
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2023-04-15 15:24:06 +0300

    add create user method in wsgi for default user and password

--- technarts_deneme_1/wsgi.py
+++ technarts_deneme_1/wsgi.py
@@ -14,3 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'technarts_deneme_1.settings')
 
 application = get_wsgi_application()
+
+
+
+
+def create_initial_user():
+    import django
+    django.setup()
+    from django.contrib.auth.models import User
+    try:
+        user = User.objects.get(username="user")
+    except  User.DoesNotExist:
+        user = User.objects.create_user("user", password="1234")
+        user.save()
+
+create_initial_user()
